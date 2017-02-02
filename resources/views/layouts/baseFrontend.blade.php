d19d362748b3868f34c5b9df3ff4ee69456be002
luisvbz <lvasquezbermudez@gmail.com>
2017-02-02 19:00:10 -0400

    Update baseFrontend.blade.php
    Add fabook API js

--- resources/views/layouts/baseFrontend.blade.php
+++ resources/views/layouts/baseFrontend.blade.php
@@ -251,6 +251,24 @@ License URL: http://creativecommons.org/licenses/by/3.0/
 		});
       
 	</script>
+	  <script>
+	  window.fbAsyncInit = function() {
+	    FB.init({
+	      appId      : '1422490531150653',
+	      xfbml      : true,
+	      version    : 'v2.8'
+	    });
+	    FB.AppEvents.logPageView();
+	  };
+
+	  (function(d, s, id){
+	     var js, fjs = d.getElementsByTagName(s)[0];
+	     if (d.getElementById(id)) {return;}
+	     js = d.createElement(s); js.id = id;
+	     js.src = "//connect.facebook.net/en_US/sdk.js";
+	     fjs.parentNode.insertBefore(js, fjs);
+	   }(document, 'script', 'facebook-jssdk'));
+	</script>
 
   @yield('js')
     
